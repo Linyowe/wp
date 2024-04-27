88033b36f54602521e914becdfeaf266677467ea
Linyowe <160702529+Linyowe@users.noreply.github.com>
2024-04-27 21:34:05 +0800

    Create 寫一個函數做矩陣相乘matrixMul(a,b).js

---
+++ javascript.js/題目7/寫一個函數做矩陣相乘matrixMul(a,b).js
@@ -0,0 +1,20 @@
+function matrixmul(a,b){
+  if(a[0].length != b.length) return null;
+  let result[];
+  for (let i=0;i<a.length;i++)
+    {
+      let row[];
+      for (let j=0;j<b[0].length;j++){
+        let sum=0;
+        for (let k=0;k<a[0].length;k++){
+          sum +=a[i][k]*b[i][j];
+        }
+        row.push(sum);
+      }
+      result.push(row);
+    }
+    return result;
+}
+
+        
+      
