0763fe1cff2ca7747442b051cce2522c7bec0e54
Linyowe <160702529+Linyowe@users.noreply.github.com>
2024-05-10 22:08:35 +0800

    Create 請寫一個 arrayMin(a) 函數傳回一個裡最小的那個數字.js

---
+++ JavaScript練習2/請寫一個 arrayMin(a) 函數傳回一個裡最小的那個數字.js
@@ -0,0 +1,15 @@
+function arrayMin(arr) {
+    if (arr.length === 0) {
+        return; 
+    }
+    var min = arr[0]; 
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+var numbers = [5, 3, 9, 1, 7];
+console.log("最小的數字是:", arrayMin(numbers));
